602c5553917b32c2f030237575baccb51cc4c26d
ARNAU CARRAU <arnau.carrau@epia.granollers>
2021-11-30 17:50:05 +0100

    Close #2 Close #8

--- gitHubEx.cpp
+++ gitHubEx.cpp
@@ -1,11 +1,13 @@
-#include "sdl.h"
 #include "sdl_image.h"
+#include "sdl.h"
 
 int main(int argc, char* args[]) {
 
 	SDL_Window* window = NULL;
 	SDL_Renderer* render = NULL;
-	SDL_Texture* texture = NULL;
+
+
+	SDL_Init(SDL_INIT_EVERYTHING);
 
 	window = SDL_CreateWindow("SDL Tutorial",
 		SDL_WINDOWPOS_UNDEFINED,
@@ -13,9 +15,35 @@ int main(int argc, char* args[]) {
 		854, 480,
 		SDL_WINDOW_SHOWN);
 
-
 	render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
 
 	SDL_SetRenderDrawColor(render, 255, 255, 255, 0);
 
+	bool endgame = false;
+	SDL_Event Tecla;
+
+	while (endgame==false){
+		SDL_PollEvent(&Tecla);
+		switch (Tecla.type)
+		{
+		case SDL_KEYDOWN:
+
+			if (Tecla.key.keysym.scancode == SDL_SCANCODE_1) {
+
+				SDL_SetRenderDrawColor(render, 255, 0, 0, 0);
+			}
+			if (Tecla.key.keysym.scancode == SDL_SCANCODE_2) {
+
+			}
+			break;
+		case SDL_QUIT:
+			endgame = true;
+			break;
+		}
+		SDL_RenderClear(render);
+		SDL_RenderPresent(render);
+		SDL_GetError();
+	}
+	return 0;
 }
